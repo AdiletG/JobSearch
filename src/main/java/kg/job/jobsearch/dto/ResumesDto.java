75f14055e4728c1bade32798b0a7763fa57b13eb
AdiletG <adiletg844@gmail.com>
2026-03-16 00:34:13 +0600

    commit-4

--- src/main/java/kg/job/jobsearch/dto/ResumesDto.java
+++ src/main/java/kg/job/jobsearch/dto/ResumesDto.java
@@ -1,10 +1,16 @@
 package kg.job.jobsearch.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResumesDto {
     private int id;
     private int applicant_id;
@@ -14,4 +20,18 @@ public class ResumesDto {
     private boolean is_active;
     private LocalDateTime created_date;
     private LocalDateTime update_time;
+
+    @Override
+    public String toString() {
+        return "ResumesDto{" +
+                "id=" + id +
+                ", applicant_id=" + applicant_id +
+                ", name='" + name + '\'' +
+                ", category_id=" + category_id +
+                ", salary=" + salary +
+                ", is_active=" + is_active +
+                ", created_date=" + created_date +
+                ", update_time=" + update_time +
+                '}';
+    }
 }
